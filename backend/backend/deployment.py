40f30a380a0de0f080bb6ca31e8a2663d2987337
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-03-13 17:15:36 +0800

    Update os.environ typo

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -2,10 +2,10 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ('WEBSITE_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ('WEBSITE_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('WEBSITE_HOSTNAME')]
 DEBUG = False
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',    
@@ -32,7 +32,7 @@ STORAGES = {
     },
 }
 # Database
-CONNECTION = os.environ("AZURE_DATABASE_CONNECTIONSTRING")
+CONNECTION = os.environ.get("AZURE_DATABASE_CONNECTIONSTRING")
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 DATABASES = {
     "default": {
